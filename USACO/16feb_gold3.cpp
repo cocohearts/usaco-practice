01bb99b739c9ed267c9436eac837cbfbabf05058
ALex Zhao <alex.zhao.superman@gmail.com>
2023-12-04 16:48:31 -0800

    20:00 first learning ROF then dealing with indices ugh

---
+++ USACO/16feb_gold3.cpp
@@ -0,0 +1,158 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using db = double;
+using str = string;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using pd = pair<db, db>;
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vd = vector<db>;
+using vs = vector<str>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vpd = vector<pd>;
+
+#define mp make_pair
+#define f first
+#define s second
+
+// const int MOD = 1e9 + 7;
+// const int MX = 2e5 + 5;
+// const int INF = 1e9;
+// const ll INF = 1e18;  // not too close to LLONG_MAX
+// const ld PI = acos((ld)-1);
+// const int dx[4] = {1, 0, -1, 0},
+//           dy[4] = {0, 1, 0, -1};  // for every grid problem!!
+
+#define sz(x) int(x.size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pb push_back
+#define eb emplace_back
+#define pf push_front
+
+#define lb lower_bound
+#define ub upper_bound
+
+#define endl "\n"
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define trav(a, x) for (auto &a : x)
+
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+constexpr int pct(int x) { return (int)__builtin_popcount(x); }
+constexpr int bits(int x) {
+	return x == 0 ? 0 : 31 - (int)__builtin_clz(x);
+}
+ 
+template <typename T, typename... U>
+void dbgh(const T& t, const U&... u) {
+    cerr << t;
+    ((cerr << " | " << u), ...);
+    cerr << endl;
+}
+ 
+void setPrec() { cout << fixed << setprecision(15); }
+void unsyncIO() { cin.tie(0)->sync_with_stdio(0); }
+
+void setIn(str s) { freopen(s.c_str(), "r", stdin); }
+void setOut(str s) { freopen(s.c_str(), "w", stdout); }
+void setIO(str s = "") {
+	unsyncIO();
+	setPrec();
+	cin.exceptions(cin.failbit);
+
+	if (sz(s)) setIn(s + ".in"), setOut(s + ".out");
+}
+
+#ifdef LOCAL
+	const int L = 100;
+	#define dbg(...)                                           \
+		cerr << "L" << __LINE__ << " [" << #__VA_ARGS__ << "]" \
+			<< ": ";                                           \
+		dbgh(__VA_ARGS__)
+#else
+	const int L = 2000;
+	#define dbg(...)
+#endif
+
+struct DSU {
+	vi g;
+	DSU(int N) {g = vi(N,-1);}
+	int get(int x) {return g[x]<0 ? x : get(g[x]);}
+	int size(int x) {return -1*g[get(x)];}
+	bool join(int x, int y) {
+		int a=get(x), b=get(y);
+		if (a==b) return false;
+		if (g[a]>g[b]) swap(a,b);
+		g[a] += g[b]; g[b] = a;
+		return true;
+	}
+};
+
+int Aarr[L+1], Barr[L+1];
+
+void solve() {
+	int A,B,n,m;
+	cin >> A >> B >> n >> m;
+	F0R(i,n) cin >> Aarr[i];
+	F0R(i,m) cin >> Barr[i];
+	sort(Aarr,Aarr+n);
+	sort(Barr,Barr+m);
+	Aarr[n] = A; Barr[m] = B;
+	R0F(ind,n) {
+		Aarr[ind+1] = Aarr[ind+1]-Aarr[ind];
+	}
+	R0F(ind,m) {
+		Barr[ind+1] = Barr[ind+1]-Barr[ind];
+	}
+	priority_queue<pair<int,pi>,vector<pair<int,pi>>,greater<pair<int,pi>>> pairings;
+	F0R(x,n+1) F0R(y,m+1) {
+		int ind = (n+1)*y+x;
+		if (x!=0) {
+			pairings.push(mp(Barr[y],mp(ind-1,ind)));
+		}
+		if (y!=0) {
+			pairings.push(mp(Aarr[x],mp(ind-n-1,ind)));
+		}
+	}
+	DSU myDSU((m+1)*(n+1));
+	ll score=0;
+	while (myDSU.size(0)!=(m+1)*(n+1)) {
+		auto nn = pairings.top(); pairings.pop();
+		if (myDSU.join(nn.s.f,nn.s.s)) {
+			score += nn.f;
+		}
+	}
+	cout << score;
+}
+
+int main() {
+	#ifdef LOCAL // call with -DLOCAL
+		freopen("../input.txt", "r", stdin);
+		freopen("../myoutput.txt", "w", stdout);
+		freopen("../debug.txt", "w", stderr);
+	#else
+		setIO("fencedin");
+	#endif
+
+	solve();
+}
