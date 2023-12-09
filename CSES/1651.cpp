a39bcb464b1cd5e74eb5c8a2c174a9d3e687f945
ALex Zhao <alex.zhao.superman@gmail.com>
2023-12-09 14:16:38 -0800

    first segtr..

---
+++ CSES/1651.cpp
@@ -0,0 +1,157 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using db = double;
+using str = string;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using pd = pair<db, db>;
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vd = vector<db>;
+using vs = vector<str>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vpd = vector<pd>;
+
+#define mp make_pair
+#define f first
+#define s second
+
+// const int MOD = 1e9 + 7;
+// const int MX = 2e5 + 5;
+// const int INF = 1e9;
+// const ll INF = 1e18;  // not too close to LLONG_MAX
+// const ld PI = acos((ld)-1);
+// const int dx[4] = {1, 0, -1, 0},
+//           dy[4] = {0, 1, 0, -1};  // for every grid problem!!
+
+#define sz(x) int(x.size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pb push_back
+#define eb emplace_back
+#define pf push_front
+
+#define lb lower_bound
+#define ub upper_bound
+
+#define endl "\n"
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define trav(a, x) for (auto &a : x)
+
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+constexpr int pct(int x) { return (int)__builtin_popcount(x); }
+constexpr int bits(int x) {
+	return x == 0 ? 0 : 31 - (int)__builtin_clz(x);
+}
+ 
+template <typename T, typename... U>
+void dbgh(const T& t, const U&... u) {
+    cerr << t;
+    ((cerr << " | " << u), ...);
+    cerr << endl;
+}
+ 
+void setPrec() { cout << fixed << setprecision(15); }
+void unsyncIO() { cin.tie(0)->sync_with_stdio(0); }
+
+void setIn(str s) { freopen(s.c_str(), "r", stdin); }
+void setOut(str s) { freopen(s.c_str(), "w", stdout); }
+void setIO(str s = "") {
+	unsyncIO();
+	setPrec();
+	cin.exceptions(cin.failbit);
+
+	if (sz(s)) setIn(s + ".in"), setOut(s + ".out");
+}
+
+#ifdef LOCAL
+	const int L = 100;
+	#define dbg(...)                                           \
+		cerr << "L" << __LINE__ << " [" << #__VA_ARGS__ << "]" \
+			<< ": ";                                           \
+		dbgh(__VA_ARGS__)
+#else
+	const int L = 100000;
+	#define dbg(...)
+#endif
+
+template <class T> struct SegTree {
+	const T DEF = 0;
+	vector<T> sgtr;
+	int len;
+
+	SegTree(int N) : len(N), sgtr(N*2,DEF) {}
+
+	void set(int ind, T val) {
+		sgtr[ind+len]=val;
+	}
+
+	void modify(int l, int r, T val) {
+		for (l+=len, r+=len; l<r; l>>=1, r>>=1) {
+			if (l&1) {
+				sgtr[l++]+=val;
+			}
+			if (r&1) {
+				sgtr[--r]+=val;
+			}
+		}
+	}
+
+	T get(int ind) {
+		T sum=0;
+		for(ind+=len; ind>0; ind>>=1) {
+			sum += sgtr[ind];
+		}
+		return sum;
+	}
+};
+
+void solve() {
+	int n,q; cin >> n >> q;
+	SegTree<ll> myTree(n);
+	F0R(i,n) {
+		cin >> myTree.sgtr[i+myTree.len];
+	}
+	int a,b,u,k;
+	F0R(_,q) {
+		int id; cin >> id;
+		if (id==1) {
+			cin >> a >> b >> u;
+			myTree.modify(a-1,b,u);
+		} else {
+			cin >> k;
+			cout << myTree.get(k-1) << endl;
+		}
+	}
+}
+
+int main() {
+	#ifdef LOCAL // call with -DLOCAL
+		freopen("../input.txt", "r", stdin);
+		freopen("../myoutput.txt", "w", stdout);
+		freopen("../debug.txt", "w", stderr);
+	#else
+		setIO();
+	#endif
+
+	solve();
+}
