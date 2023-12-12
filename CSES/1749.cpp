e132f37515f4bb8ebf02cd88bfaa7a5791eccb04
ALex Zhao <alex.zhao.superman@gmail.com>
2023-12-11 19:10:39 -0800

    another pbds nuke bruh

---
+++ CSES/1749.cpp
@@ -0,0 +1,149 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using db = double;
+using str = string;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using pd = pair<db, db>;
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vd = vector<db>;
+using vs = vector<str>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vpd = vector<pd>;
+
+#define mp make_pair
+#define f first
+#define s second
+
+// const int MOD = 1e9 + 7;
+// const int MX = 2e5 + 5;
+// const int INF = 1e9;
+// const ll INF = 1e18;  // not too close to LLONG_MAX
+// const ld PI = acos((ld)-1);
+// const int dx[4] = {1, 0, -1, 0},
+//           dy[4] = {0, 1, 0, -1};  // for every grid problem!!
+
+#define sz(x) int(x.size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pb push_back
+#define eb emplace_back
+#define pf push_front
+
+#define lb lower_bound
+#define ub upper_bound
+
+#define endl "\n"
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define trav(a, x) for (auto &a : x)
+
+// template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template <class T>
+using Tree =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int pct(int x) { return (int)__builtin_popcount(x); }
+constexpr int bits(int x) {
+	return x == 0 ? 0 : 31 - (int)__builtin_clz(x);
+}
+ 
+template <typename T, typename... U>
+void dbgh(const T& t, const U&... u) {
+    cerr << t;
+    ((cerr << " | " << u), ...);
+    cerr << endl;
+}
+ 
+void setPrec() { cout << fixed << setprecision(15); }
+void unsyncIO() { cin.tie(0)->sync_with_stdio(0); }
+
+void setIn(str s) { freopen(s.c_str(), "r", stdin); }
+void setOut(str s) { freopen(s.c_str(), "w", stdout); }
+void setIO(str s = "") {
+	unsyncIO();
+	setPrec();
+	cin.exceptions(cin.failbit);
+
+	if (sz(s)) setIn(s + ".in"), setOut(s + ".out");
+}
+
+#ifdef LOCAL
+	const int L = 100;
+	#define dbg(...)                                           \
+		cerr << "L" << __LINE__ << " [" << #__VA_ARGS__ << "]" \
+			<< ": ";                                           \
+		dbgh(__VA_ARGS__)
+#else
+	const int L = 200000;
+	#define dbg(...)
+#endif
+
+struct SegTree {
+	vi sgtr; int len;
+	SegTree(int N) {
+		len = N; sgtr = vi(2*N,0);
+	}
+	void update(int ind1, int ind2, int val) {
+		for(ind1+=len, ind2+=len; ind2>ind1; ind1<<=1, ind2<<=1) {
+			if (ind1%2) sgtr[ind1++] += val;
+			if (ind2%2) sgtr[--ind2] += val;
+		}
+	}
+	int get(int ind) {
+		int ret = 0;
+		for (ind+=len; ind>0; ind <<=1) {
+			ret += sgtr[ind];
+		}
+		return ret;
+	}
+};
+
+int nums[L];
+int removed[L];
+
+void solve() {
+	int n; cin >> n;
+	F0R(i,n) cin >> nums[i];
+	F0R(i,n) {cin >> removed[i]; --removed[i];}
+	Tree<int> ordered_set;
+	F0R(i,n) ordered_set.insert(i);
+	F0R(i,n) {
+		if (i) cout << " ";
+		cout << nums[*ordered_set.find_by_order(removed[i])];
+		ordered_set.erase(ordered_set.find_by_order(removed[i]));
+	}
+}
+
+int main() {
+	#ifdef LOCAL // call with -DLOCAL
+		freopen("../input.txt", "r", stdin);
+		freopen("../myoutput.txt", "w", stdout);
+		freopen("../debug.txt", "w", stderr);
+	#else
+		setIO();
+	#endif
+
+	solve();
+}
